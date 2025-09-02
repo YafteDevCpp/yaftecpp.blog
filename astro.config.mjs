c448c4b26ef1c90ddc3471b2a5f1644c3a0ee57d
YafteDevCpp <yafte@ciencias.unam.mx>
2025-09-01 23:02:15 -0600

    Cambio deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const options = {
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yaftedevcpp.github.io/',
+	site: 'https://yaftecpp.github.io/yaftecpp.blog',
 	base: 'yaftecpp.blog',
 
 	markdown: {
